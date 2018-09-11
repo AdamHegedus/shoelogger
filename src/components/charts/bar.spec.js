e4db63bcb84bdc0fd136144b5562a4a9ab2322d3
Adam Hegedus <Adam_Hegedus@epam.com>
2018-09-11 17:15:49 +0200

    Updated snapshot test for bar-chart

--- src/components/charts/bar.spec.js
+++ src/components/charts/bar.spec.js
@@ -2,8 +2,40 @@ import Vue from 'vue';
 import Bar from '@/components/charts/bar';
 
 describe('index.vue', () => {
+
+    const shoes = [
+        {
+            id: 1,
+            brand: 'Foo',
+            product: 'Shoe A',
+            distance: 100
+        },
+        {
+            id: 2,
+            brand: 'Foo',
+            product: 'Shoe B',
+            distance: 50
+        },
+        {
+            id: 3,
+            brand: 'Bar',
+            product: 'Shoe C',
+            distance: 0
+        },
+        {
+            id: 4,
+            brand: 'Bar',
+            product: 'Shoe D',
+            distance: 15
+        }
+    ];
+
     const Constructor = Vue.extend(Bar);
-    const vm = new Constructor().$mount();
+    const vm = new Constructor({
+        propsData: {
+            data: shoes
+        }
+    }).$mount(document.createElement('div'));
 
     it('should match snapshot', () => {
         // ASSIGN
