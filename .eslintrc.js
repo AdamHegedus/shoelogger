48fc3447c018c975296c63b353c2997a4a29f631
Adam Hegedus <limitlessaudio@gmail.com>
2018-11-21 22:03:10 +0100

    Fixes linting issues with jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,16 @@ module.exports = {
         parser: 'babel-eslint'
     },
     env: {
-        browser: true
+        browser: true,
+        "jest/globals": true
     },
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     extends: ['plugin:vue/essential', 'airbnb-base'],
     // required to lint *.vue files
     plugins: [
-        'vue'
+        'vue',
+        'jest'
     ],
     // check if imports actually resolve
     settings: {
