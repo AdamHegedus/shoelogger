ef2d445640ef922414a29b60d7eb1dd53f138eb8
Adam Hegedus <limitlessaudio@gmail.com>
2018-12-16 23:03:19 +0100

    Add unit test for logs/overview

---
+++ src/components/logs/overview.spec.js
@@ -0,0 +1,90 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Overview from '@/components/logs/overview';
+
+jest.mock('../charts/bar', () => {
+    return {
+        render: () => { 
+            return jest.fn().mockImplementation(() => { 
+                return <div class="mock-bar-chart"/>; 
+            });
+        }
+    };
+});
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('overview.vue', () => {
+    describe('when has shoes', () => {
+        it('should render component', () => {
+            // ASSIGN
+            const navigationAction = {
+                setNavigation: jest.fn()
+            };
+
+            const shoesState = {
+                shoes: [0, 1, 2, 3]
+            };
+
+            const store = new Vuex.Store({
+                modules: {
+                    navigation: {
+                        namespaced: true,
+                        actions: navigationAction
+                    },
+                    shoes: {
+                        namespaced: true,
+                        state: shoesState
+                    }
+                }
+            });
+
+            const wrapper = shallowMount(Overview, { store, localVue });
+
+            // ACT
+            const actual = wrapper.element;
+
+            // ASSERT
+            expect(actual).toMatchSnapshot();
+            expect(navigationAction.setNavigation.mock.calls.length).toMatchSnapshot();
+            expect(navigationAction.setNavigation.mock.calls[0][1]).toMatchSnapshot();
+        });
+    });
+
+    describe('when doesn\'t have shoes', () => {
+        it('should render component', () => {
+            // ASSIGN
+            const navigationAction = {
+                setNavigation: jest.fn()
+            };
+
+            const shoesState = {
+                shoes: []
+            };
+
+            const store = new Vuex.Store({
+                modules: {
+                    navigation: {
+                        namespaced: true,
+                        actions: navigationAction
+                    },
+                    shoes: {
+                        namespaced: true,
+                        state: shoesState
+                    }
+                }
+            });
+
+            const wrapper = shallowMount(Overview, { store, localVue });
+
+            // ACT
+            const actual = wrapper.element;
+
+            // ASSERT
+            expect(actual).toMatchSnapshot();
+            expect(navigationAction.setNavigation.mock.calls.length).toMatchSnapshot();
+            expect(navigationAction.setNavigation.mock.calls[0][1]).toMatchSnapshot();
+        });
+    });
+});
